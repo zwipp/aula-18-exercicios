5384fe0cb10316b4bc3b423a9ce9e3a256ddda1d
zwipp <nuneszwipp@hotmail.com>
2019-07-15 08:51:34 -0300

    Exercicio-1

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <ol>
+        <li>
+            <p> 1. Utilizando um for, imprimir os números de 1 a 100. </p>
+            <div>
+                <?php for($i = 1; $i <= 100; $i++){
+                    echo $i . " ";
+                } ?>
+            </div>
+        </li>
+    </ol>
+</body>
+</html>
