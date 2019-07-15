9d8a63e23680c3e5b944ea695238cbcde12cedbe
zwipp <nuneszwipp@hotmail.com>
2019-07-15 09:15:32 -0300

    Exercicio-3

--- index.php
+++ index.php
@@ -25,6 +25,36 @@
                 } ?>
             </div>
         </li>
+
+        <li>
+            <p> 3. Mostrar a tabela de multiplicação do 2 utilizando um for </p>
+            <div>
+                <table>
+                    <?php for($i = 0; $i <= 10; $i++){
+                        echo('<tr>');
+                        echo(' <td>');
+                        echo (2 . " x " . $i . " = " .(2*$i));
+                        echo(' </td>');
+                        echo('</tr>');
+                    } ?>
+                </table>
+            </div>
+        </li>
+
+        <li>
+            <p> 4. Utilizando um while, fazer um programa que lance uma moeda (escolhendo um número aleatório que pode ser 0 ou 1) até tirar 5 vezes cara (o número 1). Ao terminar, imprimir o número de lançamentos da moeda até tirar 5 vezes cara. </p>
+            <div>
+                <table>
+                    <?php for($i = 0; $i <= 10; $i++){
+                        echo('<tr>');
+                        echo(' <td>');
+                        echo (2 . " x " . $i . " = " .(2*$i));
+                        echo(' </td>');
+                        echo('</tr>');
+                    } ?>
+                </table>
+            </div>
+        </li>
     </ol>
 </body>
 </html>
