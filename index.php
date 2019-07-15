eabb514e5be80c73f8ac46ace1fe3cc2c1773b0b
zwipp <nuneszwipp@hotmail.com>
2019-07-15 11:12:23 -0300

    Exercicio-6-e-7

--- index.php
+++ index.php
@@ -78,6 +78,68 @@
                 ?>
             </div>
         </li>
+
+        <li>
+            <p> 6. Definir um array com 5 strings que sejam nomes. Percorrer esse array para imprimir todos os nomes na tela. <br>
+                a. Resolver este problema com um for. <br>
+                b. Resolver este problema com while. <br>
+                c. Resolver este problema com um do/while. <br>
+            </p>
+            <div>
+                <?php 
+                    
+                    $nomes = [
+                        'Sergio',
+                        'Thomaz',
+                        'Ana',
+                        'Joao',
+                        'Luci',
+                    ];
+                    
+                    echo 'A- ';
+                    for ($i=0; $i < count($nomes); $i++) { 
+                        echo $nomes[$i] . '<br>';
+                    }
+
+                    echo '<br>' . 'B-';
+                    $i = 0;
+                    while($i<count($nomes)){
+                        echo $nomes[$i] . '<br>';
+                        $i++;
+                    }
+
+                    echo '<br>' . 'C-';
+                    $i = count($nomes)-1;
+                    do {
+                        echo $nomes[$i] . '<br>';
+                        $i--;
+                    } while ($i >= 0);
+                ?>
+            </div>
+        </li>
+
+        <li>
+            <p> 7. Definir um array com 10 números aleatórios entre 0 e 10. Percorrer esse array para imprimir todos os números. A execução deve terminar se algum dos números encontrados for 5 (a mensagem impressa deve ser “Encontramos um 5!”). <br>
+            a. Resolver este problema com um for. <br>
+            b. Resolver este problema com um while. <br>
+            c. Resolver este problema com um do/while. <br>
+ </p>
+            <div>
+                <?php 
+                $array = [];
+                    for ($i=0; $i < 10; $i++) { 
+                        $array[$i] = mt_rand(0,10);
+                    }
+
+                    for ($i=0; $i < count($array); $i++) { 
+                        echo $array[$i] . '<br>';
+                        if($array[$i] == 5){
+                            break;
+                        }
+                    }
+                ?>
+            </div>
+        </li>
     </ol>
 </body>
 </html>
